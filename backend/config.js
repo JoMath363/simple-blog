92ca8632512b48e2cb5945336d30eea766aa1d08
José Mathias <mathias260405@gamil.com>
2024-10-16 20:13:19 -0300

    stop tracking config

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 
-'mongodb+srv://mathias:40028922@simple-blog-db.34fvv.mongodb.net/posts-collection?retryWrites=true&w=majority&appName=Simple-Blog-DB'
