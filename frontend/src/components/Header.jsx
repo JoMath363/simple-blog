70b25867c4b6f1e504b70fbc4fbcb13d9cfc0832
José Mathias <mathias260405@gamil.com>
2024-10-17 12:20:52 -0300

    minor changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,10 +4,10 @@ import '../styles/header.css'
 function Header() {
     return(
         <header>
-            <div id="logo">
+            <a href="/" id="logo">
                 <img src={blogIcon} alt="blog logo"/>
                 <h2>Simple Blog</h2>
-            </div>
+            </a>
 
             <a href="/login" id='login_btn'>Post</a>
         </header>
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -9,7 +9,7 @@ function Post(params) {
     return(
         <a href={`/post/${params._id}`} className="post">
             <h2>{params.title}</h2>
-            <p>{params.content}</p>
+            <p>{params.content.slice(0, 200) + '...'}</p>
             <div className="status">
                 <div className="rate-btn">
                     <img src={likeIcon} alt="Like" />
