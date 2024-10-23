7df1ea87e1024921ce12294497c2649af358ac21
José Mathias <mathias260405@gamil.com>
2024-10-23 17:05:56 -0300

    admin page done

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,6 +1,56 @@
+import Header from '../components/Header';
+import '../styles/page-styles/admin-page.css'
+
 function AdminPage() {
-    return(<>
-        <h1>Admin</h1>
+    const addNewPost = async (e) => {
+        e.preventDefault();
+
+        const title = document.getElementById('title_input');
+        const content = document.getElementById('content_input');
+
+        const postData = {
+            title: title.value,
+            content: content.value,
+            likes: '0',
+            dislikes: '0'
+        };
+
+        try {
+            const response = await fetch('http://localhost:5555/posts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(postData)
+            });
+
+            title.value = ''
+            content.value = ''
+
+            if (response.ok) {
+                alert('Post submitted successfully!');
+            } else {
+                alert('Failed to submit post');
+            }
+        } catch (error) {
+            alert('Error: ' + error.message);
+        }
+    }
+
+    return (<>
+        <Header />
+        <main id='admin_page'>
+            <form id="admin_container">
+                <h1>Add New Post</h1>
+                <label htmlFor="title_input">Title:</label>
+                <input id="title_input" type="text" required />
+
+                <label htmlFor="content_input">Content:</label>
+                <textarea id="content_input" type="text" required />
+
+                <button onClick={(e) => addNewPost(e)}>Post</button>
+            </form>
+        </main>
     </>)
 }
 
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,31 @@
 import '../styles/page-styles/login-page.css'
 
 function LoginPage() {
+
+    async function validate_login(params) {
+        const user = document.getElementById('user_input').value
+        const password = document.getElementById('password_input').value
+
+        const response = await fetch(`http://localhost:5555/login?user=${user}&password=${password}`);
+        if (response.status == 200){
+            window.location.href = '/admin';
+        } else {
+            alert('Usuário ou senha incorretos.');
+        }
+    }
+
+
     return(<>
         <main id='login_page'>
             <section id="login_container">
                 <h1>Login</h1>
                 <label htmlFor="user_input">Usuário:</label>
-                <input id="user_input"/>
+                <input id="user_input" type="text"/>
 
                 <label htmlFor="password_input">Senha:</label>
-                <input id="password_input"/>
+                <input id="password_input" type="password"/>
 
-                <button>Entrar</button>
+                <button onClick={() => validate_login()}>Enter</button>
             </section>
         </main>
     </>)
